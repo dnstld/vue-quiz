dde2bd4c5054dc953ebc15fe9b8bfcc005c6c49c
Denis Toledo <contato@denistoledo.com.br>
2020-03-28 16:46:07 -0300

    add boostrap + start components

---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="question-box-container">
+    <b-jumbotron>
+      <template v-slot:lead>
+       Some question here?
+      </template>
+
+      <hr class="my-4">
+
+      <p>
+        List of answers
+      </p>
+
+      <b-button variant="primary" href="#" class="m-2">Submit</b-button>
+      <b-button variant="success" href="#" class="m-2">Next</b-button>
+    </b-jumbotron>
+  </div>
+</template>
