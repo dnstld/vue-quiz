d3df6e237cfe8a4fc9923683e7013c1efeeb7156
Denis Toledo <contato@denistoledo.com.br>
2020-03-28 18:54:53 -0300

    shuffle answers with lodash

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -11,6 +11,8 @@
         <b-list-group-item
           v-for="(answer, index) in answers"
           :key="index"
+          @click="selectAnswer(index)"
+          :class="[selectedIndex === index ? 'selected' : '']"
         >
           {{ answer }}
         </b-list-group-item>
@@ -23,16 +25,58 @@
 </template>
 
 <script>
+  import _ from 'lodash';
+
   export default {
     props: {
       currentQuestion: Object,
       next: Function
+    },
+    data() {
+      return {
+        selectedIndex: null,
+        shuffledAnswers: []
+      }
     },
     computed: {
       answers() {
         const answers  = [...this.currentQuestion.incorrect_answers, this.currentQuestion.correct_answer];
         return answers;
       }
+    },
+    watch: {
+      currentQuestion: {
+        immediate: true,
+        handler() {
+          this.selectedIndex = null;
+          this.shuffleAnswers();
+        }
+      }
+    },
+    methods: {
+      selectAnswer(index) {
+        this.selectedIndex = index;
+      },
+      shuffleAnswers() {
+        const answers  = [...this.currentQuestion.incorrect_answers, this.currentQuestion.correct_answer];
+        this.shuffledAnswers = _.shuffle(answers);
+      }
     }
   }
 </script>
+
+<style scoped>
+  .list-group-item:hover {
+    background-color: #eee;
+    cursor: pointer;
+  }
+  .selected {
+    background-color: lightblue;
+  }
+  .correct {
+    background-color: lightgreen;
+  }
+  .incorrect {
+    background-color: lightsalmon;
+  }
+</style>
